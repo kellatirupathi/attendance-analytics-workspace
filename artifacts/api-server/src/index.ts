6159e1a443184009cfa1e7d77dc3a00c3d9e0509
hariniv-eng <harini.v@nxtwave.co.in>
2026-08-23 00:48:51 +0530

    Bind API server to all interfaces

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -23,7 +23,7 @@ async function start(): Promise<void> {
     logger.error({ err }, "Startup seed failed (continuing)");
   }
 
-  app.listen(port, (err) => {
+  app.listen(port, "0.0.0.0", (err) => {
     if (err) {
       logger.error({ err }, "Error listening on port");
       process.exit(1);
